0700dba00d955f7564a3a977b712505ea9146909
Dreampop <161981890+energypop@users.noreply.github.com>
2024-12-29 21:41:42 +0100

    Removing UFW case

--- vsftpd_installer.sh
+++ vsftpd_installer.sh
@@ -22,22 +22,6 @@ $package_manager vsftpd
 echo "Turning ftp server on"
 
 sudo systemctl enable --now vsftpd 
-
-echo "UFW firewall can cause issues. Do you want to disable it (y), add vsftpd to the exceptions list (e) or keep UFW on (on)"
-
-read -r decision
-
-if [ "$decision" = "y" ]; 
-then
-    sudo ufw disable
-fi
-
-
-if [ "$decision" = "e" ]; 
-then
-    sudo ufw allow vsftpd
-fi
-
 echo "Connecting to FTP server"
 echo "Remember to activate 'write enable' on the configuration file. You can use any FTP client from now on. Goodbye"
 
